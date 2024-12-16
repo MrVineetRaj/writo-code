226cad4dbca0ecf9ca453ce853a5ea8eb9cfe79e
Vineet Raj <vineetrajrj26@gmail.com>
2024-12-16 12:38:33 +0530

    Add request parameter to GET route and log request body

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -55,7 +55,9 @@ export async function POST(req: Request) {
   }
 }
 
-export async function GET() {
+export async function GET(req: Request) {
+
+  console.log("GET request",req.body);
   try {
     await connectToDatabase();
     console.log("Connected to database");
